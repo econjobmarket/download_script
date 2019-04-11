45a1ad034cf827c45113e43a7e504ed115bf55bb
mike <peters@econ.ubc.ca>
2019-04-10 20:22:39 -0700

    fixed the script so that it correctly revises all the files and attributes

--- app/update_cron.php
+++ app/update_cron.php
@@ -19,6 +19,7 @@ if ($_SERVER ['HTTP_HOST'])
   $link = new db;
   $link -> addConnection($cfg -> database);
   $link -> setAsGlobal();
+  $u = new DownloadUtility($cfg);
  // $autoloader = require... then $result = $autoloader -> findFile('Econjobmarket\OauthClient');
   $started_at = date("Y-m-d H:i:s");
   print "Starting script at ".$started_at."\n";
@@ -28,8 +29,117 @@ if ($_SERVER ['HTTP_HOST'])
     print "Error creating client:".$client -> error."\nDescription: ".$client -> error_description;
     die("No Curl client, stopped without processing");
   } else {
-    //starting with appications, note the insert or update method so entries are always replaced
-    print "Processing applications\n";
+    // check applicants first so we can retrospectively record gender with applications
+    
+    print "Start by fetching applicants:\n";
+    $result = $client -> fetch('applicants');
+    if($result) {
+      
+      foreach($result as $res) {
+        if($showdots) print("*");
+        db::table('myapplicants')
+        -> where('aid', $res -> aid)
+        -> updateOrInsert([
+            'aid' => $res -> aid,
+            'enrolldate' => $res -> enrolldate,
+            'changedate' => $res -> changedate,
+            'email' => $res -> email,
+            'fname' => $res -> fname,
+            'mname' => $res -> mname,
+            'lname' => $res -> lname,
+            'country' => $res -> country,
+            'phone' => $res -> phone,
+            'fax' => $res -> fax,
+            'url' => $res -> url,
+            'category_id' => $res -> category_id,
+            'primary_field' => $res -> primary_field,
+            'degreetype' => $res -> degreetype,
+            'degreeinst' => $res -> degreeinst,
+            'degreebegyear' => $res -> degreebegyear,
+            'degreeendyear' => $res -> degreeendyear,
+            'curpos' => $res -> curpos,
+            'curinst' => $res -> curinst,
+            'ethnic' => $res -> ethnic,
+            'ethnicity' => $res -> ethnicity,
+            'race' => $res -> race,
+            'gender' => $res -> gender,
+            'cv_url' => $res -> cv_url,
+            'ackdate' => $res -> ack_date,
+            'nationality' => $res -> nationality,
+            'statusinactivedate' => $res -> statusinactivedate,
+            'last_revised_time' => $res -> last_revised_time,
+        ]
+            );
+        //make sure the gender has been recorded in my_sorting_criteria
+        if(db :: table ( 'my_sorting_criteria' )
+            -> where ( 'criteria', $res -> gender )
+            -> doesntExist ()) {
+              //add the criteria
+              if($res -> gender and (strlen($res -> gender) > 0))
+              db::table('my_sorting_criteria')
+                -> insert([
+                    'criteria' => $res -> gender,
+                    'gender' => 1,
+                ]);
+            }
+            
+        //add the secondary fields
+        foreach($res -> secondary_fields as $key => $value) {
+          db::table('my_secondary_fields')
+          -> where([
+              ['aid', $res -> aid],
+              ['category_id', $key]
+          ])
+          -> updateOrInsert([
+              'aid' => $res -> aid,
+              'category_id' => $key,
+              'name' => $value,
+          ]);
+          // add the secondary fields if they aren't there
+          if(db :: table ( 'my_sorting_criteria' )
+              -> where ( 'field_id', $key )
+              -> doesntExist ()) {
+                //add the criteria
+                  db::table('my_sorting_criteria')
+                  -> insert([
+                      'criteria' => $value,
+                      'field_id' => $key,
+                  ]);
+              }
+              
+        }
+        //now the conferences
+        foreach($res -> conferences as $key => $value) {
+          db::table('myconferences')
+          -> where([
+              ['aid', $res -> aid],
+              ['conference_id', $key],
+          ])
+          -> updateOrInsert([
+              'aid' => $res -> aid,
+              'conference_id' => $key,
+              'conference_name' => $value,
+          ]);
+          if(db :: table ( 'my_sorting_criteria' )
+              -> where ( 'conference_id', $key )
+              -> doesntExist ()) {
+                //add the criteria
+                db::table('my_sorting_criteria')
+                -> insert([
+                    'criteria' => $value,
+                    'conference_id' => $key,
+                ]);
+              }
+              
+        }
+      }
+      print "\nAfter applicants using ".memory_get_usage()." with peak ".memory_get_peak_usage()."\n";
+    } else print "Nothing returned fetching applicants\n";
+    
+    
+      
+      //starting with appications, note the insert or update method so entries are always replaced
+    print "Processing applications now\n";
     $result = $client -> fetch('applications');
     if($result -> error == 'invalid request' and ($result -> error_description == 'Missing or expired token.')) {
       unset($client);
@@ -50,6 +160,54 @@ if ($_SERVER ['HTTP_HOST'])
                 'adtitle' => $res -> adtitle,
                 ]
                 );
+            // mark the gender of the application
+            $criteria_id = $u -> getGenderId($res -> aid);
+            if(db :: table ( 'my_criteria_assignment' )
+                -> where ( [['criteria_id', $criteria_id],
+                    ['appid', $res -> appid]]
+                    )
+                -> doesntExist ()) {
+                  //add the criteria
+                  db::table('my_criteria_assignment')
+                  -> insert([
+                      'appid' => $res -> appid,
+                      'criteria_id' => $criteria_id,
+                  ]);
+                }
+                // next add the secondary to mark the applications
+                $fields = $u -> getFields($res -> aid);
+                foreach($fields as $field) {
+                if(db :: table ( 'my_criteria_assignment' )
+                    -> where ( [['criteria_id', $field],
+                        ['appid', $res -> appid]]
+                        )
+                    -> doesntExist ()) {
+                      //add the criteria
+                      db::table('my_criteria_assignment')
+                      -> insert([
+                          'appid' => $res -> appid,
+                          'criteria_id' => $field,
+                      ]);
+                    }
+                }
+                    
+                // final step add the conferences
+                $conferences = $u -> getConferences($res -> aid);
+                foreach($conferences as $conference) {
+                  if(db :: table ( 'my_criteria_assignment' )
+                      -> where ( [['criteria_id', $conference],
+                          ['appid', $res -> appid]]
+                          )
+                      -> doesntExist ()) {
+                        //add the criteria
+                        db::table('my_criteria_assignment')
+                        -> insert([
+                            'appid' => $res -> appid,
+                            'criteria_id' => $conference,
+                        ]);
+                      }
+                }
+                
             foreach($res -> questions as $question) {
               db::table('myquestions') 
                 -> where([
@@ -68,6 +226,33 @@ if ($_SERVER ['HTTP_HOST'])
                     'updated_at' => $question -> updated_at,
                     'response_text' => $question -> response_text,
                 ]);
+                //save the sorting criteria if it isn't there
+                if(db :: table ( 'my_sorting_criteria' )
+                    -> where ( 'response_id', $question -> response_id )
+                    -> doesntExist ()) {
+                      //add the criteria
+                      db::table('my_sorting_criteria')
+                      -> insert([
+                          'criteria' => $question -> response_text,
+                          'response_id' => $question -> response_id,
+                          'posid' => $res -> posid,
+                      ]);
+                    }
+                 //now save the assignment
+                    $criteria_id = $u -> getSortingIdFromResponseId($question -> response_id);
+                    if(db :: table ( 'my_criteria_assignment' )
+                        -> where ( [['criteria_id', $criteria_id],
+                            ['appid', $res -> appid]]
+                        )
+                        -> doesntExist ()) {
+                          //add the criteria
+                          db::table('my_criteria_assignment')
+                          -> insert([
+                              'appid' => $res -> appid,
+                              'criteria_id' => $criteria_id,
+                          ]);
+                        }
+                        
             }
 // now files
             foreach($res -> files as $file) {
@@ -114,76 +299,7 @@ if ($_SERVER ['HTTP_HOST'])
       }
       print "\nAfter applications used ".memory_get_usage()." with peak ".memory_get_peak_usage()."\n";
     } else print "Nothing returned fetching applications\n";
-    print "Now fetching applicants:\n";
-    $result = $client -> fetch('applicants');
-    if($result) {
-
-        foreach($result as $res) {
-          if($showdots) print("*");
-          db::table('myapplicants')
-            -> where('aid', $res -> aid)
-            -> updateOrInsert([
-                'aid' => $res -> aid,
-                'enrolldate' => $res -> enrolldate,
-                'changedate' => $res -> changedate,
-                'email' => $res -> email,
-                'fname' => $res -> fname,
-                'mname' => $res -> mname,
-                'lname' => $res -> lname,
-                'country' => $res -> country,
-                'phone' => $res -> phone,
-                'fax' => $res -> fax,
-                'url' => $res -> url,
-                'category_id' => $res -> category_id,
-                'primary_field' => $res -> primary_field,
-                'degreetype' => $res -> degreetype,
-                'degreeinst' => $res -> degreeinst,
-                'degreebegyear' => $res -> degreebegyear,
-                'degreeendyear' => $res -> degreeendyear,
-                'curpos' => $res -> curpos,
-                'curinst' => $res -> curinst,
-                'ethnic' => $res -> ethnic,
-                'ethnicity' => $res -> ethnicity,
-                'race' => $res -> race,
-                'gender' => $res -> gender,
-                'cv_url' => $res -> cv_url,
-                'ackdate' => $res -> ack_date,
-                'nationality' => $res -> nationality,
-                'statusinactivedate' => $res -> statusinactivedate,
-                'last_revised_time' => $res -> last_revised_time,
-            ]
-                );
-            //add the secondary fields
-            foreach($res -> secondary_fields as $key => $value) {
-            db::table('my_secondary_fields')
-                -> where([
-                    ['aid', $res -> aid],
-                    ['category_id', $key]
-                ])
-                -> updateOrInsert([
-                    'aid' => $res -> aid,
-                    'category_id' => $key,
-                    'name' => $value,
-                ]);
-            }
-            //now the conferences
-            foreach($res -> conferences as $key => $value) {
-                db::table('myconferences')
-                -> where([
-                    ['aid', $res -> aid],
-                    ['conference_id', $key],
-                ])
-                -> updateOrInsert([
-                    'aid' => $res -> aid,
-                    'conference_id' => $key,
-                    'conference_name' => $value,
-                ]);
-            }
-        }
-        print "\nAfter applicants using ".memory_get_usage()." with peak ".memory_get_peak_usage()."\n";
-    } else print "Nothing returned fetching applications\n";
-   
-    //final section will update blobs
+       //final section will update blobs
     // check myfiles and myrecommendation tables then verify whether or not the blobs have 
     // already been downloaded.
     //Then clean the blobs table by deleting any blobs that don't have a matching file or recommendation record
--- packages/Econjobmarket/DownloadUtility.php
+++ packages/Econjobmarket/DownloadUtility.php
@@ -1,8 +1,72 @@
 <?php
-namespace  Econjobmarket\DownloadUtility;
+namespace  Econjobmarket;
 
-final class DownloadUtility {
+use \Illuminate\Database\Capsule\Manager;
+class db extends \Illuminate\Database\Capsule\Manager{};
 
+
+class DownloadUtility {
+  public function __contruct($cfg) {
+    $this ->link = new db;
+    $this -> link -> addConnection($cfg -> database);
+    $this -> link -> setAsGlobal();
+  }
+  public function get_sorting_id($criteria) {
+    // $criteria would be a text string like Male - this would find the id used for my_criteria_assignment
+    $id = db:: table('my_sorting_criteria') 
+      -> where('criteria', $criteria)-> value('criteria_id') ;
+    if($id) return $id;
+    else return false;
+  }
+  public function getSortingIdFromResponseId($response_id) {
+    $id = db:: table('my_sorting_criteria')
+    -> where('response_id', $response_id)-> value('criteria_id') ;
+    if($id) return $id;
+    else return false;
+    
+  }
+  //from an applicant aid, get their gender and its corresponding criteria_id
+  public function getGenderId($aid) {
+    $gender = db::table('myapplicants')
+     -> where('aid', $aid) -> value('gender');
+     if($gender) {
+       $criteria_id = $this -> get_sorting_id($gender);
+       if($criteria_id) return $criteria_id;
+       return false;
+     }
+    return false;
+  }
+  public function getSortingIdFromFieldId($field_id) {
+    $id = db::table('my_sorting_criteria')
+      -> where('field_id', $field_id) -> value('criteria_id');
+    if($id) return $id;
+    return false;
+  }
+  public function getSortingIdFromConferenceId($conference_id) {
+    $id = db::table('my_sorting_criteria')
+    -> where('conference_id', $conference_id) -> value('criteria_id');
+    if($id) return $id;
+    return false;
+  }
+  
+  public function getFields($aid) {
+    $fields = array();
+    $field_ids = db::table('my_secondary_fields')
+    -> where('aid', $aid) -> pluck('category_id');
+    foreach($field_ids as $f) {
+      $fields[] = $this -> getSortingIdFromFieldId($f);
+    }
+    return $fields;
+  }
+  public function getConferences($aid) {
+    $conferences = array();
+    $conference_ids = db::table('myconferences')
+    -> where('aid', $aid) -> pluck('conference_id');
+    foreach($conference_ids as $c) {
+      $conferences[] = $this -> getSortingIdFromConferenceId($c);
+    }
+    return $conferences;
+  }
 }
 
 ?>
